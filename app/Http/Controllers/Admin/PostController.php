4824b86975e104dcbe64262f70bba5702688706e
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-05 13:09:08 +0100

    Slug(ciclo infinito)

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Post;
 
 class PostController extends Controller
@@ -26,7 +27,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -37,7 +38,26 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $form_data = $request->all();
+        $new_post = new Post();
+        $new_post->fill($form_data);
+
+        //GENERO LO SLUG TRAMITE UNA CLASSE LARAVEL STR E I SUOI METODI
+        $slug = str::slug($new_post['title'], '-');
+        // VERIFICO SE LO SLUG SIA UNICO NEL SUO GENERE POICHE NEL DATABASE L HO IMPOSTATO COME UNICO
+        $slug_presente = Post::where('slug', $slug)->first();
+        // ISTANZIO UN CONTATORE NUMERICO CHE VERRÁ AGGIUNTO AL MIO SLUG BASE CON UN CICLO WHILE ANDANDO AD INCREMENTARE IL VALORE QUAL ORA ESSO SIA GIA PRESENTE
+        $contatore = 1;
+        while ($slug_presente) {
+            $slug = $slug . '-' . $contatore;
+            //VERIFICO SE NON HO UN POST CON LO STESSO SLUG ANCHE ALL INTERNO
+            $slug_presente = Post::where('slug', $slug);
+            $contatore++;
+        }
+
+        $new_post->slug = $slug;
+        $new_post->save();
+        return redirect()->route('admin.posts.index')->with('status', 'Post inserito correttamente');
     }
 
     /**
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,10 +1,47 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <ul>
+    {{-- <ul>
         @foreach ($posts as $post)
             {{--    POSSO COMPORRE LA ROTTA CON L'ID, MA IN QUESTO CASO LO SLUG E' PIU PARLANTE QUINDI UTILIZZO LO SLUG, ESSENDO ANCHE ESSO UNIVOCO --}}
-            <li><a href="{{ route('admin.posts.show', $post['id']) }}">{{ $post['title'] }}</a></li>
-        @endforeach 
-    </ul>
+            {{-- <li><a href="{{ route('admin.posts.show', $post['id']) }}">{{ $post['title'] }}</a></li> --}}
+        {{-- @endforeach  --}}
+    {{-- </ul> --}}
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td scope="row">{{ $post['id'] }}</td>
+                                <td>{{ $post['title'] }}</td>
+                                <td>{{ $post['slug'] }}</td>
+                                <td>
+                                    <a href="{{ route('admin.posts.show', $post['id']) }}" class="btn btn-info">Details</a>
+                                    <a href="{{ route('admin.posts.edit', $post['id']) }}" class="btn btn-warning">Edit</a>
+                                    <form action="" class="d-inline-block" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                  </table>
+            </div>
+        </div>
+    </div>
+    
 @endsection
