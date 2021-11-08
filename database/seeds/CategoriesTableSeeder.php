27d03bdb5fecfdb92bf5359dba1294725bb7b017
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-08 16:13:42 +0100

    foreign key column

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -18,9 +18,8 @@ class CategoriesTableSeeder extends Seeder
         for ($i = 0; $i < count($categories); $i++) {
             $new_category = new Category();
             $new_category->name = $categories[$i];
-            $new_category->slug = Str::slug($categories[$i]);
-        }
-
-        
+            $new_category->slug = Str::slug($categories[$i], '-');
+            $new_category->save();
+        }   
     }
 }
