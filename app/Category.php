2e06a783636fdc77419c286980696e1fcd7ae3bf
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-08 16:21:00 +0100

    model link

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // COLLEGO TRAMITE METODO LA TABELLA PRINCIPALE ALLA SUBORDINATA
+    protected $fillable = ['title', 'slug', 'category_id'];
+    public function posts() {
+        return $this->hasMany('App\Post');
+    }
 }
