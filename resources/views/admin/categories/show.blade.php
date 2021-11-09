c7f5abc857453ef3a44d85f7b4bfe02e7e57d3d5
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-09 15:07:03 +0100

    Show categories

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,6 +8,17 @@
                 <h2>{{ $category['name'] }}</h2>
                 <small>Lo slug è: {{ $category['slug'] }}</small>
             </div>
+            {{-- VADO A STAMPARE TUTTI I POST COLLEGATI ALLA CATEGORIA  E RENDO POSSIBILE VEDERNE I DETTAGLI --}}
+            <div class="col-12 mt-4">
+                <h2>Lista dei post collegati alla Categoria</h2>
+                <ul>
+                    @forelse ($category->posts as $post)
+                        <li><a href="{{ route('admin.posts.show', $post['id'])}}">{{ $post['title'] }}</a></li>
+                    @empty
+                        <p>Nessun post collegato</p>
+                    @endforelse
+                </ul>
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,8 +7,9 @@
                 <h1>Visualizzazione Post {{ $post['id'] }}</h1>
                 <h2>{{ $post['titolo'] }}</h2>
                 <p>{!! $post['content'] !!}</p>
-
+                
                 <small>Lo slug è: {{ $post['slug'] }}</small>
+                <small class="d-block">Categoria di appartenenza: <a href="{{ route('admin.categories.show', $post->category['id']) }}">{{ $post->category->name }}</a></small>
             </div>
         </div>
     </div>
