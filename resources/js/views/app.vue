379611bccafe74206801c9745c2dfcbe18014ac6
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-11-12 12:56:43 +0100

    import header e main

---
+++ resources/js/views/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <Header />
+        <Main />
+    </div>
+</template>
+
+<script>
+import '../components/Header.vue';
+import '../components/Main.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Header,
+        Main,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
